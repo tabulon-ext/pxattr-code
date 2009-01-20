38efb737c679a473a2cc034ad522a4cdf6a8d999
dockes <none@none>
2009-01-20 12:05:41 +0000

    ok?

--- pxattr.cpp
+++ pxattr.cpp
@@ -442,6 +442,7 @@ bool pxname(nspace dom, const string& sname, string* pname)
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <string.h>
 
@@ -454,6 +455,7 @@ static char usage [] =
 "trpxattr [-h] -n name [-v value] pathname...\n"
 "trpxattr [-h] -x name pathname...\n"
 "trpxattr [-h] -l pathname...\n"
+"trpxattr -T: run tests on temp file in current directory" 
 "\n"
 ;
 static void
@@ -470,6 +472,129 @@ static int     op_flags;
 #define OPT_h     0x8
 #define OPT_x     0x10
 #define OPT_l     0x20
+#define OPT_T     0x40
+
+static void dotests()
+{
+    static const char *tfn = "pxattr_testtmp.xyz";
+    static const char *NAMES[] = {"NAME1", "N2", "LONGGGGGGGGGGGisSSSHHHHHHHHHNAME3"};
+    static const char *VALUES[] = {"VALUE1", "VALUE2", "VALUE3"};
+    static bool verbose = true;
+
+    /* Create test file if it doesn't exist, remove all attributes */
+    int fd = open(tfn, O_RDWR|O_CREAT, 0755);
+    if (fd < 0) {
+	perror("open/create");
+	exit(1);
+    }
+
+
+    if (verbose)
+	fprintf(stdout, "Cleanup old attrs\n");
+    vector<string> names;
+    if (!pxattr::list(tfn, &names)) {
+	perror("pxattr::list");
+	exit(1);
+    }
+    for (vector<string>::const_iterator it = names.begin(); 
+	 it != names.end(); it++) {
+	string value;
+	if (!pxattr::del(fd, *it)) {
+	    perror("pxattr::del");
+	    exit(1);
+	}
+    }
+    /* Check that there are no attributes left */
+    names.clear();
+    if (!pxattr::list(tfn, &names)) {
+	perror("pxattr::list");
+	exit(1);
+    }
+    if (names.size() != 0) {
+	fprintf(stderr, "Attributes remain after initial cleanup !\n");
+	for (vector<string>::const_iterator it = names.begin();
+	     it != names.end(); it++) {
+	    fprintf(stderr, "%s\n", (*it).c_str());
+	}
+	exit(1);
+    }
+
+    /* Create attributes, check existence and value */
+    if (verbose)
+	fprintf(stdout, "Creating extended attributes\n");
+    for (int i = 0; i < 3; i++) {
+	if (!pxattr::set(fd, NAMES[i], VALUES[i])) {
+	    perror("pxattr::set");
+	    exit(1);
+	}
+    }
+    if (verbose)
+	fprintf(stdout, "Checking creation\n");
+    for (int i = 0; i < 3; i++) {
+	string value;
+	if (!pxattr::get(tfn, NAMES[i], &value)) {
+	    perror("pxattr::get");
+	    exit(1);
+	}
+	if (value.compare(VALUES[i])) {
+	    fprintf(stderr, "Wrong value after create !\n");
+	    exit(1);
+	}
+    }
+
+    /* Delete one, check list */
+    if (verbose)
+	fprintf(stdout, "Delete one\n");
+    if (!pxattr::del(tfn, NAMES[1])) {
+	perror("pxattr::del one name");
+	exit(1);
+    }
+    if (verbose)
+	fprintf(stdout, "Check list\n");
+    for (int i = 0; i < 3; i++) {
+	string value;
+	if (!pxattr::get(fd, NAMES[i], &value)) {
+	    if (i == 1)
+		continue;
+	    perror("pxattr::get");
+	    exit(1);
+	} else if (i == 1) {
+	    fprintf(stderr, "Name at index 1 still exists after deletion\n");
+	    exit(1);
+	}
+	if (value.compare(VALUES[i])) {
+	    fprintf(stderr, "Wrong value after delete 1 !\n");
+	    exit(1);
+	}
+    }
+
+    /* Test the CREATE/REPLACE flags */
+    // Set existing with flag CREATE should fail
+    if (verbose)
+	fprintf(stdout, "Testing CREATE/REPLACE flags use\n");
+    if (pxattr::set(tfn, NAMES[0], VALUES[0], pxattr::PXATTR_CREATE)) {
+	fprintf(stderr, "Create existing with flag CREATE succeeded !\n");
+	exit(1);
+    }
+    // Set new with flag REPLACE should fail
+    if (pxattr::set(tfn, NAMES[1], VALUES[1], pxattr::PXATTR_REPLACE)) {
+	fprintf(stderr, "Create new with flag REPLACE succeeded !\n");
+	exit(1);
+    }
+    // Set new with flag CREATE should succeed
+    if (!pxattr::set(fd, NAMES[1], VALUES[1], pxattr::PXATTR_CREATE)) {
+	fprintf(stderr, "Create new with flag CREATE failed !\n");
+	exit(1);
+    }
+    // Set existing with flag REPLACE should succeed
+    if (!pxattr::set(fd, NAMES[0], VALUES[0], pxattr::PXATTR_REPLACE)) {
+	fprintf(stderr, "Create existing with flag REPLACE failed !\n");
+	exit(1);
+    }
+    close(fd);
+    unlink(tfn);
+    exit(0);
+}
 
 static void listattrs(const string& path)
 {
@@ -532,6 +657,7 @@ int main(int argc, char **argv)
       Usage();
     while (**argv)
       switch (*(*argv)++) {
+      case 'T':	op_flags |= OPT_T; break;
       case 'l':	op_flags |= OPT_l; break;
       case 'x':	op_flags |= OPT_x; if (argc < 2)  Usage();
 	  name = *(++argv); argc--; 
@@ -547,7 +673,7 @@ int main(int argc, char **argv)
   b1: argc--; argv++;
   }
 
-  if (argc < 1)
+  if (argc < 1 && !(op_flags & OPT_T))
     Usage();
   if (op_flags & OPT_l) {
       while (argc > 0) {
@@ -567,6 +693,8 @@ int main(int argc, char **argv)
       while (argc > 0) {
 	  delxattr(*argv++, name);argc--;
       } 
+  } else if (op_flags & OPT_T)  {
+      dotests();
   }
   exit(0);
 }
